cf4da064990618b954df87eccf6dcfe1df9c3465
Nicolas Goldenberg <nicogol0@gmail.com>
2017-11-09 22:46:46 -0300

    Programming languages + Getters & Setters

--- src/Summa/EmployeesBundle/Entity/Company.php
+++ src/Summa/EmployeesBundle/Entity/Company.php
@@ -24,15 +24,15 @@ class Company
      */
     private $businessName;
 
-//    /**
-//     * @ORM\OneToMany(targetEntity="Developer", mappedBy="company")
-//     */
-//    private $developers;
-//
-//    /**
-//     * @ORM\OneToMany(targetEntity="Designer", mappedBy="company")
-//     */
-//    private $designers;
+    /**
+     * @ORM\OneToMany(targetEntity="Developer", mappedBy="company")
+     */
+    private $developers;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Designer", mappedBy="company")
+     */
+    private $designers;
 
 
     public function __construct() {
@@ -40,4 +40,106 @@ class Company
         $this->designers = new Collection();
     }
 
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set businessName
+     *
+     * @param string $businessName
+     *
+     * @return Company
+     */
+    public function setBusinessName($businessName)
+    {
+        $this->businessName = $businessName;
+
+        return $this;
+    }
+
+    /**
+     * Get businessName
+     *
+     * @return string
+     */
+    public function getBusinessName()
+    {
+        return $this->businessName;
+    }
+
+    /**
+     * Add developer
+     *
+     * @param \Summa\EmployeesBundle\Entity\Developer $developer
+     *
+     * @return Company
+     */
+    public function addDeveloper(\Summa\EmployeesBundle\Entity\Developer $developer)
+    {
+        $this->developers[] = $developer;
+
+        return $this;
+    }
+
+    /**
+     * Remove developer
+     *
+     * @param \Summa\EmployeesBundle\Entity\Developer $developer
+     */
+    public function removeDeveloper(\Summa\EmployeesBundle\Entity\Developer $developer)
+    {
+        $this->developers->removeElement($developer);
+    }
+
+    /**
+     * Get developers
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDevelopers()
+    {
+        return $this->developers;
+    }
+
+    /**
+     * Add designer
+     *
+     * @param \Summa\EmployeesBundle\Entity\Designer $designer
+     *
+     * @return Company
+     */
+    public function addDesigner(\Summa\EmployeesBundle\Entity\Designer $designer)
+    {
+        $this->designers[] = $designer;
+
+        return $this;
+    }
+
+    /**
+     * Remove designer
+     *
+     * @param \Summa\EmployeesBundle\Entity\Designer $designer
+     */
+    public function removeDesigner(\Summa\EmployeesBundle\Entity\Designer $designer)
+    {
+        $this->designers->removeElement($designer);
+    }
+
+    /**
+     * Get designers
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDesigners()
+    {
+        return $this->designers;
+    }
 }
---
+++ src/Summa/EmployeesBundle/Entity/ProgrammingLanguage.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Summa\EmployeesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * @ORM\Table("Programming_languages")
+ * @ORM\Entity
+ */
+class ProgrammingLanguage
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return ProgrammingLanguage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
